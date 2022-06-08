83a2ed4e37ae06c3b7a0d023516567b8bd9b550a
Your Name <you@example.com>
2022-06-08 13:57:09 -0400

    update walt dataset loading

--- configs/walt/walt_vehicle.py
+++ configs/walt/walt_vehicle.py
@@ -1,6 +1,6 @@
 _base_ = [
     '../_base_/models/occ_mask_rcnn_swin_fpn.py',
-    '../_base_/datasets/parking_real_coco.py',
+    '../_base_/datasets/walt_vehicle.py',
     '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
 ]
 
